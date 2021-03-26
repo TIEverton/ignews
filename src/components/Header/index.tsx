d3a4b66d288729ba1094b9c40843e3d1fc0d26f9
evertonpinheiroti@hotmail.com <evertonpinheiroti@hotmail.com>
2021-03-26 19:59:27 -0300

    :sparkles: Adding markup to which page I'm on

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,23 @@
 import { SignInButton } from '../SignInButton';
+import Link from 'next/link'
+import { useRouter } from 'next/dist/client/router';
+import { ActiveLink } from '../ActiveLink';
 import styles from './styles.module.scss';
 
 export function Header() {
+  const { asPath } = useRouter();
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink href="/" activeClassName={styles.active} >
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
