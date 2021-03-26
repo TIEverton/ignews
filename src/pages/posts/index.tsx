62ae515bf267a611f85bcce180e12bb7ca26be16
evertonpinheiroti@hotmail.com <evertonpinheiroti@hotmail.com>
2021-03-26 20:00:03 -0300

    :sparkles: Adding dynamic posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,10 +1,22 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head'
 import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
 import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss'
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -13,16 +25,14 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2020</time>
-            <strong>Lorem ipsum dolor sit amet</strong>
-            <p>Lorem ipsum dolor sit amet, consectetur adipis lorem</p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2020</time>
-            <strong>Lorem ipsum dolor sit amet</strong>
-            <p>Lorem ipsum dolor sit amet, consectetur adipis lorem</p>
-          </a>
+          {posts.map(post => (
+            <a href="#" key={post.slug}>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
+
         </div>
       </main>
     </>
@@ -39,9 +49,21 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 100,
   })
 
-  console.log(response)
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      })
+
+    }
+  })
 
   return {
-    props: {}
+    props: { posts }
   }
 }
