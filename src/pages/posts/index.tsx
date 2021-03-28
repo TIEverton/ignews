310942651916dd22b2d8719836f21bc8ba543494
evertonpinheiroti@hotmail.com <evertonpinheiroti@hotmail.com>
2021-03-27 22:13:23 -0300

    :sparkles: Adding active signature validation

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Prismic from '@prismicio/client'
 import { RichText } from 'prismic-dom'
 import { getPrismicClient } from '../../services/prismic';
+import Link from 'next/link';
 import styles from './styles.module.scss'
 
 type Post = {
@@ -26,11 +27,13 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map(post => (
-            <a href="#" key={post.slug}>
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link href={`/posts/${post.slug}`}>
+              <a key={post.slug}>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
 
         </div>
